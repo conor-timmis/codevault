54fe6798bc522f386bd3729921b6a1360bebad67
Conor Timmis <55244147+conor-timmis@users.noreply.github.com>
2025-06-17 14:29:48 +0100

    Add 404 Page

---
+++ public/404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 Not Found</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="auth-form">
+            <h1>404 - Page Not Found</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+            <a href="index.php" class="btn btn-primary">Go Home</a>
+        </div>
+    </div>
+</body>
+</html>
