3f6f4d529c11f0f0ead21928e139da8efde501c7
Conor Timmis <55244147+conor-timmis@users.noreply.github.com>
2025-06-17 14:30:19 +0100

    Add router for non-existent pages

---
+++ public/router.php
@@ -0,0 +1,18 @@
+<?php
+// router.php
+
+// This file is used by the PHP built-in server to route requests
+
+$requested = $_SERVER["REQUEST_URI"];
+$file = __DIR__ . '/public' . parse_url($requested, PHP_URL_PATH);
+
+if (php_sapi_name() === 'cli-server') {
+    if (is_file($file)) {
+        return false;
+    } else {
+        // Serve custom 404 page
+        http_response_code(404);
+        require __DIR__ . '/public/404.php';
+        return true;
+    }
+}
